d0802a98e086df6f721518c18f31f66ced8815bb
Leonardo Rufino <marcosldrufino1@gmail.com>
2023-09-30 17:27:12 -0300

    commit

---
+++ JavaLoops/src/pacote/one.java
@@ -0,0 +1,12 @@
+package pacote;
+
+public class one {
+public static void main(String[] args) {
+	for (int i = 1; i <5 ; i++) {
+		int j = 0;
+		while (j < i) {
+			System.out.print(j + " ");
+		}
+	}
+}
+}
---
+++ JavaLoops/src/pacote/two.java
@@ -0,0 +1,14 @@
+package pacote;
+
+public class two {
+public static void main(String[] args) {
+	
+int i = 5;
+while (i >= 1) {
+	int num = 1;
+	for (int j = 1; j <= i; j++) {
+		System.out.print(num + "XXX");
+	}
+}
+}
+}
